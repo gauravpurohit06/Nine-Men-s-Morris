f615348477dd31581e2594307b088bdaa261337f
Vaibhav Agarwal <vaibhavagarwal220@gmail.com>
2018-07-01 18:49:27 +0530

    added more styling and background to board and the webpage

--- konva-testing/makeBoard.js
+++ konva-testing/makeBoard.js
@@ -28,15 +28,28 @@
     var test = {'x1':(rectDim[0].x1+rectDim[0].x2)/2,'y1':rectDim[0].y2,'x2':(rectDim[0].x1+rectDim[0].x2)/2,'y2': rectDim[2].y2};
     lineDim.push(test);
 
+
+    var p1Pieces=[];
+    var p2Pieces=[];
+
     for(i=0;i<9;i++)
     {
        var circle = new Konva.Circle({
         x: 0.1*width + i*30,
         y: 0.4*height + (i%3)*30,
-        radius: height/35,
-        fill: 'green'
+        radius: (width+height)/135,
+           fillRadialGradientStartPoint: 0,
+            fillRadialGradientStartRadius: 0,
+            fillRadialGradientEndPoint: 0,
+            fillRadialGradientEndRadius: (width+height)/135,
+            fillRadialGradientColorStops: [0, '#87ceeb',1,'#45b3e0'],shadowColor: 'black',
+            shadowBlur: 0,
+            shadowOffset: {x : 5, y : 8},
+            shadowOpacity: 0.5
       });
        layer.add(circle);
+       p1Pieces.push(circle);
+       p1Pieces[i].on('click', function (){ alert(i+'th Listener'); });
     }
 
         for(i=0;i<9;i++)
@@ -44,10 +57,20 @@
        var circle = new Konva.Circle({
         x: 0.7*width + i*30,
         y: 0.4*height + (i%3)*30,
-        radius: height/35,
-        fill: 'red'
+        radius: (width+height)/135, 
+	    fillRadialGradientStartPoint: 0,
+	    fillRadialGradientStartRadius: 0,
+	    fillRadialGradientEndPoint: 0,
+	    fillRadialGradientEndRadius: (width+height)/135,
+	    fillRadialGradientColorStops: [0, '#CD5C5C', 1, '#DC143C'],
+	    shadowColor: 'black',
+            shadowBlur: 0,
+            shadowOffset: {x : 5, y : 8},
+            shadowOpacity: 0.5
+
       });
        layer.add(circle);
+       p1Pieces.push(circle);
     }
 
 
@@ -58,8 +81,8 @@
     var line = new Konva.Line
       ({
         points: [lineDim[i].x1, lineDim[i].y1, lineDim[i].x2, lineDim[i].y2],
-        stroke: 'black',
-        strokeWidth: 2,
+        stroke: 'white',
+        strokeWidth: 6,
         lineCap: 'round',
         lineJoin: 'round'
       });
@@ -82,16 +105,11 @@
         y: rectDim[i].y1,
         width: rectDim[i].x2 - rectDim[i].x1,
         height: rectDim[i].y2 - rectDim[i].y1,
-        //fill: 'green',
-        stroke: 'black',
-        strokeWidth: 2
+        stroke: 'white',
+        strokeWidth: 6
       });
       rectArray.push(rect);
       layer.add(rect);
-
-
-
-
 
        var circle = new Konva.Circle({
         x: rectDim[i].x1,
@@ -100,6 +118,9 @@
         fill: colorCircle
       });
        layer.add(circle);
+
+       //circle.on('click', function (){ alert('First Listener'); });
+
         
         var circle = new Konva.Circle({
         x: (rectDim[i].x1+rectDim[i].x2)/2,
@@ -157,7 +178,28 @@
       });
        layer.add(circle);
     }
- 
 
-    stage.add(layer);
+
+    var imageObj = new Image();
+    imageObj.onload = function() {
+
+    var bgr = new Konva.Image({
+        image: imageObj,
+        x: rectDim[2].x1,
+        y: rectDim[2].y1,
+                    shadowBlur: 0,
+            shadowOffset: {x : 15, y : 15},
+            shadowOpacity: 0.5,
+
+        width: rectDim[2].x2 - rectDim[2].x1,
+        height: rectDim[2].y2 - rectDim[2].y1
+      });
+
+      layer.add(bgr);
+      bgr.moveToBottom();
+      stage.add(layer);
+    };
+    imageObj.src = '/NMM/konva-testing/assets/chalk.png';
+
+ 
  
